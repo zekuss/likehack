78633f688dfac6d62056e0c409de9b1196381da0
Denis Zakusilo <DVZakusilo@gmail.com>
2018-05-02 20:17:07 +0300

    PushAll + новый логотип

---
+++ bitrix/php_interface/init.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ZeK
+ * Date: 02.05.2018
+ * Time: 19:31
+ */
+
+AddEventHandler("iblock", "OnAfterIBlockElementAdd", Array("Handlers", "OnAfterIBlockElementAddHandler"));
+
+class Handlers
+{
+    // создаем обработчик события "OnAfterIBlockElementAdd"
+    function OnAfterIBlockElementAddHandler(&$arFields)
+    {
+        if ($arFields['IBLOCK_ID'] == 1 && !empty($arFields['RESULT'])) {
+
+            $el_res= CIBlockElement::GetByID( $arFields['RESULT'] );
+            if( $el_arr = $el_res->GetNext()){
+                $arFields['DETAIL_PAGE_URL'] = $el_arr['DETAIL_PAGE_URL'];
+            }
+
+//             Отправляем в pushall
+            $ch = curl_init();
+            curl_setopt_array($ch, array(
+                CURLOPT_URL => "https://pushall.ru/api.php",
+                CURLOPT_POSTFIELDS => array(
+                    "type" => "broadcast",
+                    "id" => "4328",
+                    "key" => "b06eaae0f5ed80d30bc572f9983186db",
+                    "text" => $arFields['PREVIEW_TEXT'],
+                    "title" => $arFields['NAME'],
+                    "url" => 'http://likehack.ru' . $arFields['DETAIL_PAGE_URL']
+                ),
+                CURLOPT_SAFE_UPLOAD => true,
+                CURLOPT_RETURNTRANSFER => true
+            ));
+            $return=json_decode(curl_exec($ch)); //получить ответ или ошибку
+            curl_close($ch);
+        }
+    }
+}
--- bitrix/templates/lifehack-main/footer.php
+++ bitrix/templates/lifehack-main/footer.php
@@ -6,8 +6,14 @@
     <div class="container">
         <div class="inside">
             <div class="wrapper">
-                <div class="fleft"></div>
-                <div class="aligncenter">© likehack.ru, 2018—<?= date('Y') ?></div>
+                <div class="col-xs-6">
+                    <iframe frameborder="0" src="https://pushall.ru/widget.php?subid=4328" width="320" height="120"
+                            scrolling="no" style="overflow: hidden;">
+                    </iframe>
+                </div>
+                <div class="col-xs-6">
+                    <div class="aligncenter">© likehack.ru, 2018—<?= date('Y') ?></div>
+                </div>
             </div>
         </div>
     </div>
